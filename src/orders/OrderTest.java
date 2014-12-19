90cd7f575cbaadd83663dedec60d36d4e757fc78
regimental12 <richard.hankins@gmail.com>
2014-12-19 14:08:32 +0000

    Junit Test Waiting

--- src/orders/OrderTest.java
+++ src/orders/OrderTest.java
@@ -4,12 +4,17 @@ import catalogue.Basket;
 import catalogue.Product;
 import middle.*;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.System.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 /**
@@ -24,6 +29,50 @@ public class OrderTest
     private Basket theBought2 = null;        // items
     private StockReadWriter theStock = null; // list
     private OrderProcessing theOrder = null; //
+
+    public Boolean checkOrderState(int orderNo , String key) throws OrderException {
+        //take in key to search
+        //loop through to find key
+        /*
+        Map<String, List<String>> m = new HashMap<String, List<String>>();
+
+        m.put("list1", Arrays.asList("s1", "s2", "s3"));
+
+        for (Map.Entry<String, List<String>> me : m.entrySet()) {
+          String key = me.getKey();
+          List<String> valueList = me.getValue();
+          System.out.println("Key: " + key);
+          System.out.print("Values: ");
+          for (String s : valueList) {
+            System.out.print(s + " ");
+          }
+        }*/
+        // once found the key loop through those values
+        try
+        {
+            Map check = theOrder.getOrderState();
+            for(Map.Entry<String, List<Integer>> loop :theOrder.getOrderState().entrySet())
+            {
+                String keyToCheck = loop.getKey();
+                if (keyToCheck == key)
+                {
+                    List<Integer> values = loop.getValue();
+                    for(int i : values)
+                    {
+                        if (i == orderNo)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        catch (OrderException e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
     @Before
     public void setUp()
@@ -68,6 +117,10 @@ public class OrderTest
             theOrder.newOrder( theBought1 );  // ORDER_NUMBER1
             theOrder.newOrder( theBought2 );  // ORDER_NUMBER2
             // Check if "Waiting" ORDER_NUMBER1 & ORDER_NUMBER2
+            System.out.println(theOrder.getOrderState().toString());
+            Assert.assertTrue(checkOrderState(ORDER_NUMBER1,"Waiting"));
+            Assert.assertTrue(checkOrderState(ORDER_NUMBER2,"Waiting"));
+
 
             //Get order to pick
             Basket o2p = theOrder.getOrderToPick();
@@ -77,6 +130,7 @@ public class OrderTest
 
             //Check [order number 1] completion of being "picked"
             ok = theOrder.informOrderPicked( ORDER_NUMBER1 );
+
 
             //Check if [order number 3]can be completed
             // This should fail as no [order no 3]
@@ -98,6 +152,7 @@ public class OrderTest
             ok = theOrder.informOrderPicked( ORDER_NUMBER2 );
 
             ok = theOrder.informOrderColected( ORDER_NUMBER2 );
+
         } catch ( Exception e )
         {
             fail( "Exception " + e.getMessage() );
---
+++ src/orders/OrderTesting.java
@@ -0,0 +1,51 @@
+package orders;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderTesting extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testUniqueNumber() throws Exception {
+
+    }
+
+    @Test
+    public void testNewOrder() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOrderToPick() throws Exception {
+
+    }
+
+    @Test
+    public void testInformOrderPicked() throws Exception {
+
+    }
+
+    @Test
+    public void testInformOrderColected() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOrderState() throws Exception {
+
+    }
+}
